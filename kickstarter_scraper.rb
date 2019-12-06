7281ddbd72850cad8e433d5d4d2b75e7810ebb64
Garrett Thompson <gtcruz.gt@gmail.com>
2019-12-06 01:08:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,7 @@
 require 'nokogiri'
 require 'pry'
 
+# projects: kickstarter.css("kickstarter_scraper.rb")
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
