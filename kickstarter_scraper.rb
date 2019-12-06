5cbd4250f845a5bafd08c62fe8ddcc5e24eecaf4
Garrett Thompson <gtcruz.gt@gmail.com>
2019-12-06 01:17:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,7 @@ require 'pry'
 ########### adding comments ###############
 # projects: kickstarter.css("kickstarter_scraper.rb")
 # title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
 
 def create_project_hash
   # write your code here
